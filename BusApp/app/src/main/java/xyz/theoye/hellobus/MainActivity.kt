1dfccd6ad67628ebec84c6a86d6442ca007567d3
TheoYe <theoye888@gmail.com>
2020-06-21 20:45:34 +0800

    finish login layout

---
+++ BusApp/app/src/main/java/xyz/theoye/hellobus/MainActivity.kt
@@ -0,0 +1,12 @@
+package xyz.theoye.hellobus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
